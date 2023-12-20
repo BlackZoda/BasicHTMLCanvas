4ba68d49936d1e0fe54b3aa62c11515e1e02c2f1
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2023-12-20 15:18:53 +0100

    Floating particles

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
-//window.addEventListener('resize', drawRect);
-window.addEventListener('resize', drawCircle);
+const particlesArray = [];
 
 const mouse = {
-    xClick: undefined,
-    yClick: undefined,
-    xMove: undefined,
-    yMove: undefined
+    x: undefined,
+    y: undefined
 }
-canvas.addEventListener('click', event => {
-    mouse.xClick = event.x;
-    mouse.yClick = event.y;
-});
 
 canvas.addEventListener('mousemove', event => {
     mouse.xMove = event.x;
@@ -26,25 +19,46 @@ function setCanvas() {
 }
 setCanvas();
 
-function drawRect() {
-    setCanvas();
-    ctx.fillStyle = 'white';
-    ctx.fillRect(200, 200, 150, 50); // x, y, width, height
+class Particle {
+    constructor() {
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    draw() {
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+        ctx.fill();
+    }
 }
 
-function drawCircle(x, y) {
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'yellow';
-    ctx.lineWidth = 10;
-    ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
-    ctx.fill();
-    ctx.stroke();
+function initParticlesArray() {
+    for (let i = 0; i < 100; i++) {
+        particlesArray.push(new Particle());
+    }
+}
+initParticlesArray();
+console.log(particlesArray);
+
+function handleParticles() {
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
 }
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCircle(mouse.xMove, mouse.yMove);
+    handleParticles();
     requestAnimationFrame(animate);
 }
 animate();
